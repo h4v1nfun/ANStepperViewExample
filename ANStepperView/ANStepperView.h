006c1ac5a8aae5cd78d22e99d5eb20d1cc2110ab
Afonso Cavaco Neto <afonso.neto@inm.pt>
2016-02-24 13:39:13 +0000

    change imports

--- ANStepperView/ANStepperView.h
+++ ANStepperView/ANStepperView.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Innovation Makers. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 typedef NS_ENUM(NSUInteger, ANStepperType) {
     ANStepperTypeDefault,
